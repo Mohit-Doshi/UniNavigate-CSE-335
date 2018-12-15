112c874a31b69d3e2127a9add76ca83863a2777c
Mohit Doshi <34958877+Mohit-Doshi@users.noreply.github.com>
2018-12-15 17:21:05 +0530

    Add files via upload

---
+++ Code/test/my_new_api_test.js
@@ -0,0 +1,2 @@
+var assert = require("chai").assert;var myNewApi = require("../lib/my_new_api.js");
+describe("myNewApi", function () { it("exports handleRequest", function () { assert.typeOf(myNewApi.handleRequest, "function"); });});
