6688441eeacca80640a7f8de0ce08b387fa0f3c6
Mohit Doshi <34958877+Mohit-Doshi@users.noreply.github.com>
2018-12-15 17:22:16 +0530

    Delete my_new_api_test.js

--- Code/test/my_new_api_test.js
+++
@@ -1,2 +0,0 @@
-var assert = require("chai").assert;var myNewApi = require("../lib/my_new_api.js");
-describe("myNewApi", function () { it("exports handleRequest", function () { assert.typeOf(myNewApi.handleRequest, "function"); });});
