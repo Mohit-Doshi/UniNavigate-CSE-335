5f2a55966f89a6a54ab3058989f5b8968351d3c6
Mohit Doshi <34958877+Mohit-Doshi@users.noreply.github.com>
2018-12-15 17:22:24 +0530

    Delete index.js

--- Code/index.js
+++
@@ -1,7 +0,0 @@
-var package = require("./package.json");
-var myNewApi = require("./lib/my_new_api.js");
-console.log("loaded " + package.name + ", version " + package.version);
-exports.handler = function (event, context) {
-myNewApi.handleRequest(event, context.done);
-}
-
